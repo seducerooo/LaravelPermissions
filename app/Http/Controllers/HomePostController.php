a26cd4231ed35645aafbb48eead9c42638412773
seducerooo <94935126+seducerooo@users.noreply.github.com>
2023-01-29 17:08:47 +0330

    Home Post Controller && Request && Model && Policy && Factory && Migration  && Seeder created also home post route were added to route service provider

---
+++ app/Http/Controllers/HomePostController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HomePost;
+use App\Http\Requests\StoreHomePostRequest;
+use App\Http\Requests\UpdateHomePostRequest;
+
+class HomePostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreHomePostRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreHomePostRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HomePost  $homePost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HomePost $homePost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HomePost  $homePost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HomePost $homePost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateHomePostRequest  $request
+     * @param  \App\Models\HomePost  $homePost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateHomePostRequest $request, HomePost $homePost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HomePost  $homePost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HomePost $homePost)
+    {
+        //
+    }
+}
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,13 +34,14 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-
-            Route::middleware('web')
                 ->group(base_path('routes/auth/authentication.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/admin/admin-panel.php'));
+
+
+            Route::middleware('web')
+                ->group(base_path('routes/home/home.php'));
         });
     }
 
