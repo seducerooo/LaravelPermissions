9863f5ea892588b01e337ea3f9dee7e608e5af6c
seducerooo <94935126+seducerooo@users.noreply.github.com>
2023-01-31 22:45:46 +0330

    two middleware were created and they added to route file authentication routes also they were added to kernel

--- routes/auth/authentication.php
+++ routes/auth/authentication.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Route;
                                              /*    */
                                           /* Register */
                                              /*    */
-Route::get('/register',[AuthenticationController::class,'register'])->name('auth.register');
+Route::get('/register',[AuthenticationController::class,'register'])->middleware('AlreadyLoggedIn')->name('auth.register');
 Route::post('/register',[AuthenticationController::class,'store'])->name('auth.store');
 
                                                /* */
                                             /* login */
                                                /* */
-Route::get('/login',[AuthenticationController::class,'login'])->name('auth.login');
+Route::get('/login',[AuthenticationController::class,'login'])->middleware('AlreadyLoggedIn')->name('auth.login');
 Route::post('/login',[AuthenticationController::class,'logged'])->name('auth.logged');
