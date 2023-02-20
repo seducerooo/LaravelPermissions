fe56d2d525f4161a62c564d2362b3ad624f350eb
seducerooo <94935126+seducerooo@users.noreply.github.com>
2023-02-20 11:38:19 +0330

    authentication table deleted it was extra also authentication model deleted in addition request && factory && seeder deleted too

--- resources/views/pages/authentication/login.blade.php
+++ resources/views/pages/authentication/login.blade.php
@@ -19,6 +19,7 @@
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
                                     <form class="user" method="POST" action="{{ route('auth.logged') }}">
+                                        @csrf
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                                         </div>
--- resources/views/pages/authentication/register.blade.php
+++ resources/views/pages/authentication/register.blade.php
@@ -13,6 +13,7 @@
                                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                             </div>
                             <form class="user" action="{{ route('auth.store') }}" method="POST">
+                                @csrf
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input name="name" type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="Name">
